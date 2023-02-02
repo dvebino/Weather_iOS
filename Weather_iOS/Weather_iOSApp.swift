b9428efeaef0d9355669ea2005a20c5a6c0060d4
dvebino <dhuang2495@gmail.com>
2023-02-02 16:44:14 -0500

    Initial Commit

---
+++ Weather_iOS/Weather_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_iOSApp.swift
+//  Weather_iOS
+//
+//  Created by David Huang on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
