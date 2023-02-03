b2e8fb0d3341a5aa1d3be514a49d595ff8d17560
dvebino <dhuang2495@gmail.com>
2023-02-03 17:21:46 -0500

    WeatherView completed -> Moving onto displaying weekly weather data

---
+++ Weather_iOS/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  Weather_iOS
+//
+//  Created by David Huang on 2/3/23.
+//
+
+import Foundation
+
+extension Double {
+    func roundDouble() -> String {
+        return String(format: "%.0f", self)
+    }
+}
--- Weather_iOS/Views/ContentView.swift
+++ Weather_iOS/Views/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
         VStack {
             if let location = locationManager.location {
                 if let weather = weather {
-                    Text("Weather Data Fetched")
+                    WeatherView(weather: weather)
                 } else {
                     LoadingView()
                         .task {
