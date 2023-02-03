b3df4d6ad52782e434eb3b00de0c0ff6fd835fae
dvebino <dhuang2495@gmail.com>
2023-02-03 14:16:12 -0500

    building location retrieval and update functions in location manager

--- Weather_iOS/Managers/LocationManager.swift
+++ Weather_iOS/Managers/LocationManager.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import CoreLocation // Framework from Apple to get the users location
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    
+    @Published var location: CLLocationCoordinate2D?
+    @Published var isLoading = false
+    
+    override init(){
+        super.init()
+        manager.delegate = self
+    }
+    
+    func requestLocation(){
+        isLoading = true
+        manager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.first?.coordinate
+        isLoading = false
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error retrieving location", error)
+        isLoading = false
+    }
+}
