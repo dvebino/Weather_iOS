cd980d332305d7669c440d2824956828d07c6d0e
dvebino <dhuang2495@gmail.com>
2023-02-06 15:41:36 -0500

    WeatherView completed - testing with simulator location

--- Weather_iOS/Views/WeatherView.swift
+++ Weather_iOS/Views/WeatherView.swift
@@ -36,7 +36,7 @@ struct WeatherView: View {
                         
                         Spacer()
                         
-                        Text(weather.main.feelsLike.roundDouble() + "°")
+                        Text(weather.main.feelsLike.roundDouble() + "°C")
                             .font(.system(size:100))
                             .fontWeight(.bold)
                             .padding()
@@ -61,6 +61,35 @@ struct WeatherView: View {
             }
             .padding()
             .frame(maxWidth: .infinity, alignment: .leading)
+            
+            VStack{
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 20){
+                    Text("Weather Now")
+                        .bold()
+                        .padding(.bottom)
+                    
+                    HStack {
+                        WeatherRow(logo: "thermometer", name: "Min temp", value: (weather.main.tempMin.roundDouble() + ("°C")))
+                        Spacer()
+                        WeatherRow(logo: "thermometer", name: "Max temp", value: (weather.main.tempMax.roundDouble() + "°C"))
+                    }
+                    
+                    HStack {
+                        WeatherRow(logo: "wind", name: "Wind speed", value: (weather.wind.speed.roundDouble() + " m/s"))
+                        Spacer()
+                        WeatherRow(logo: "humidity", name: "Humidity", value: "\(weather.main.humidity.roundDouble())%")
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .padding(.bottom, 20)
+                .foregroundColor(Color(hue: 0.522, saturation: 0.794, brightness: 0.801))
+                .background(.white)
+                .cornerRadius(20, corners: [.topLeft,.topRight])
+            }
+            
         }
         .edgesIgnoringSafeArea(.bottom)
         .background(Color(hue: 0.522, saturation: 0.794, brightness: 0.801))
